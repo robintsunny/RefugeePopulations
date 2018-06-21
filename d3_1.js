2a352b0d0dd5daefca23a957d3a090c7600f06ba
robin <robintsunny@gmail.com>
2018-06-20 23:28:53 -0700

    Add more data to render

--- d3_1.js
+++ d3_1.js
@@ -1,7 +1,7 @@
 var targetYear = 1990;
 
 function refugees(targetYear) {
-  const width = 1300,
+  const width = 1600,
     height = 750;
 
   const svg = d3
@@ -11,28 +11,26 @@ function refugees(targetYear) {
     .attr("class", "svg-container")
     .append("g")
     // .attr("transform", "translate(0,0)");
-  .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
   //      ==> this will set the initial setup at the center
 
   var defs = svg.append("defs");
 
   d3.json("./refugee_data.json", function(data) {
-  // var targetYear = 1990
-
-
+    // var targetYear = 1990
 
     // SCALE
 
     var scale = d3
       .scaleSqrt()
       // .linear()
-      .domain([1, 10000])
-      .range([1, 10]);
+      .domain([0, 5000000])
+      .range([0, 100]);
 
     var colorScale = d3
       .scaleLinear()
       // .linear()
-      .domain([1, 100000])
+      .domain([0, 1000000])
       .range(["red", "green"]);
 
     defs
@@ -61,11 +59,9 @@ function refugees(targetYear) {
       .enter()
       .append("circle")
       .attr("cx", d => {
-        console.log(targetYear)
         return scale(d.asylum[targetYear]);
       })
       .attr("cy", d => {
-
         return scale(d.asylum[targetYear]);
       })
       .attr("r", d => {
@@ -74,9 +70,9 @@ function refugees(targetYear) {
       .attr("fill", d => {
         return "url(#" + d.code + ")";
       })
-      // .attr("stroke", "red")
+      .attr("stroke", "white")
       // .attr("stroke-width", 0.5)
-      .attr("fill-opacity", 0.3)
+      .attr("fill-opacity", 0.7)
       .on("mouseover", function(d) {
         d3.select(this)
           .attr("fill-opacity", 1)
@@ -90,125 +86,120 @@ function refugees(targetYear) {
         // d3.selectAll("text.country-code").remove();
       });
 
+    d3.select("#y1990").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(1990);
+    });
+    d3.select("#y1991").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(1991);
+    });
+    d3.select("#y1992").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(1992);
+    });
+    d3.select("#y1993").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(1993);
+    });
+    d3.select("#y1994").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(1994);
+    });
+    d3.select("#y1995").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(1995);
+    });
+    d3.select("#y1996").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(1996);
+    });
+    d3.select("#y1997").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(1997);
+    });
+    d3.select("#y1999").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(1999);
+    });
+    d3.select("#y1998").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(1998);
+    });
+    d3.select("#y1999").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(1999);
+    });
+    d3.select("#y2000").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(2000);
+    });
+    d3.select("#y2001").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(2001);
+    });
+    d3.select("#y2002").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(2002);
+    });
+    d3.select("#y2003").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(2003);
+    });
+    d3.select("#y2004").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(2004);
+    });
+    d3.select("#y2005").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(2005);
+    });
+    d3.select("#y2006").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(2006);
+    });
+    d3.select("#y2007").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(2007);
+    });
+    d3.select("#y2008").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(2008);
+    });
+    d3.select("#y2009").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(2009);
+    });
+    d3.select("#y2010").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(2010);
+    });
+    d3.select("#y2011").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(2011);
+    });
+    d3.select("#y2012").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(2012);
+    });
+    d3.select("#y2013").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(2013);
+    });
+    d3.select("#y2014").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(2014);
+    });
+    d3.select("#y2015").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(2015);
+    });
+    d3.select("#y2016").on("click", () => {
+      svg.selectAll("circle").remove();
+      refugees(2016);
+    });
 
-
-
-      d3.select("#y1990").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(1990)
-      });
-      d3.select("#y1991").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(1991);
-      });
-      d3.select("#y1992").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(1992);
-      });
-      d3.select("#y1993").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(1993);
-      });
-      d3.select("#y1994").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(1994);
-      });
-      d3.select("#y1995").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(1995);
-      });
-      d3.select("#y1996").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(1996);
-      });
-      d3.select("#y1997").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(1997);
-      });
-      d3.select("#y1999").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(1999);
-      });
-      d3.select("#y1998").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(1998);
-      });
-      d3.select("#y1999").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(1999);
-      });
-      d3.select("#y2000").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(2000);
-      });
-      d3.select("#y2001").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(2001);
-      });
-      d3.select("#y2002").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(2002);
-      });
-      d3.select("#y2003").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(2003);
-      });
-      d3.select("#y2004").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(2004);
-      });
-      d3.select("#y2005").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(2005);
-      });
-      d3.select("#y2006").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(2006);
-      });
-      d3.select("#y2007").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(2007);
-      });
-      d3.select("#y2008").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(2008);
-      });
-      d3.select("#y2009").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(2009);
-      });
-      d3.select("#y2010").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(2010);
-      });
-      d3.select("#y2011").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(2011);
-      });
-      d3.select("#y2012").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(2012);
-      });
-      d3.select("#y2013").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(2013);
-      });
-      d3.select("#y2014").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(2014);
-      });
-      d3.select("#y2015").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(2015);
-      });
-      d3.select("#y2016").on("click", () => {
-        svg.selectAll("circle").remove();
-        refugees(2016);
-
-      });
-
-
-    d3.select("#split").on("click", () => {
+    d3.select("#continent").on("click", () => {
       simulation
         .force("x", forceXSplit)
         .alphaTarget(0.5)
@@ -223,19 +214,27 @@ function refugees(targetYear) {
 
     var forceXSplit = d3
       .forceX(d => {
-        if (d.code === "USA") {
-          return width * 0.25;
-        } else {
-          return width * 0.75;
+        if (d.continent === "NA") {
+          return -(width * 3) / 7;
+        } else if (d.continent === "SA") {
+          return (width * -2) / 7;
+        } else if (d.continent === "EU") {
+          return (width * -1) / 7;
+        } else if (d.continent === "AF") {
+          return 0;
+        } else if (d.continent === "AS") {
+          return (width * 1) / 7;
+        } else if (d.continent === "AU") {
+          return (width * 2) / 7;
+        } else if (d.continent === "AN") {
+          return (width * 3) / 7;
         }
       })
-      .strength(0.05);
+      .strength(0.5);
 
     var forceXCombine = d3.forceX().strength(0.05);
 
-    var forceY = d3
-      .forceY()
-      .strength(0.05);
+    var forceY = d3.forceY().strength(0.05);
 
     var forceCollide = d3.forceCollide(d => {
       return scale(d.asylum[targetYear]);
@@ -304,8 +303,6 @@ function refugees(targetYear) {
     //   });
   });
 
-
-
   // const circle1 = svg
   //   .append("circle")
   //   .attr("cx", 200)
@@ -338,4 +335,4 @@ function refugees(targetYear) {
   // var map = svg.append("g").attr("id", "map");
 }
 
-refugees(targetYear)
+refugees(1990);
